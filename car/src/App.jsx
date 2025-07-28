4d5082c1d1a7b665766b260e7af95570324f1465
ronak-pal1 <ronakpaul882@gmail.com>
2025-07-28 14:08:01 +0530

    gsap scroll animation adde

--- car/src/App.jsx
+++ car/src/App.jsx
@@ -1,4 +1,3 @@
-import { Canvas } from "@react-three/fiber";
 import Scene from "./components/Scene";
 import Header from "./components/Header";
 
@@ -6,8 +5,9 @@ const App = () => {
   return (
     <>
       <Header />
-      <div className="w-full h-screen">
         <Scene />
+
+      <div className="w-full h-screen">
 
         <div className="w-full h-full absolute inset-0 z-30 flex items-center px-7">
           <h2 className="text-9xl text-neutral-800 w-1/2">
--- car/src/components/Scene.jsx
+++ car/src/components/Scene.jsx
@@ -5,8 +5,9 @@ import {
   useGLTF,
   useHelper,
 } from "@react-three/drei";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { DirectionalLightHelper } from "three";
+import gsap from "gsap";
 
 const MoveableLights = () => {
   const lightRef = useRef();
@@ -23,9 +24,73 @@ const MoveableLights = () => {
 
 const Scene = () => {
   const { scene } = useGLTF("/models/car.glb");
+  const modelRef = useRef();
+
+  useEffect(() => {
+    console.log(innerHeight + 300);
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      console.log(scrollY);
+      if (scrollY > innerHeight / 2) {
+        gsap.to(modelRef.current.position, {
+          x: 0,
+          y: -0.5,
+          z: 0,
+          duration: 1,
+          ease: "power2.out",
+        });
+
+        gsap.to(modelRef.current.rotation, {
+          x: 0.3,
+          y: Math.PI / 2,
+          z: Math.PI * 0,
+          duration: 1,
+          ease: "power2.out",
+        });
+
+        gsap.to(modelRef.current.scale, {
+          x: 2.5,
+          y: 2.5,
+          z: 2.5,
+          duration: 1,
+          ease: "power2.out",
+        });
+      }
+
+      if(scrollY < innerHeight/3)
+      {
+         gsap.to(modelRef.current.position, {
+          x: 3,
+          y: -0.5,
+          z: 0,
+          duration: 1,
+          ease: "power2.out",
+        });
+
+        gsap.to(modelRef.current.rotation, {
+          x: 0,
+          y: -Math.PI / 3,
+          z: -Math.PI * 0.04,
+          duration: 1,
+          ease: "power2.out",
+        });
+
+        gsap.to(modelRef.current.scale, {
+          x: 2.5,
+          y: 2.5,
+          z: 2.5,
+          duration: 1,
+          ease: "power2.out",
+        });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="w-full h-full absolute top-0 left-0 z-40">
+    <div className="w-full h-full fixed top-0 left-0 z-40">
       <Canvas
         camera={{ position: [0, 0, 5] }}
         style={{ height: "100vh", width: "100vw" }}
@@ -42,11 +107,14 @@ const Scene = () => {
         />
         <directionalLight
           position={[3.491706702510397, 0.1206969541350309, 0.9999999999995772]}
-          color={"#f5f5f5"}
-          intensity={20}
+          color={"#95f462"}
+          intensity={40}
         />
 
+        <pointLight position={[0.8194705728740226, 0.7999019019161718, 0.4541680284650951,]} color={"#2479e9"} intensity={40}/>
+
         <primitive
+          ref={modelRef}
           object={scene}
           position={[3, -0.5, 0]}
           rotation={[0, -Math.PI / 3, -Math.PI * 0.04]}
